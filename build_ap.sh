a7ce19b175f0b8c193027aa2a460abd6642bba68
Austin Hester <ahester57@gmail.com>
2019-11-01 04:57:09 +0000

    init commit
    Signed-off-by: Austin Hester <ahester57@gmail.com>

---
+++ build_ap.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+sudo add-apt-repository "deb http://archive.ubuntu.com/ubuntu $(lsb_release -sc) universe" &&
+sudo apt update &&
+sudo apt install bash util-linux procps hostapd iproute2 iw wireless-tools haveged iptables dnsmasq git openpvn &&
+git clone https://github.com/oblique/create_ap.git
+cd create_ap &&
+sudo make install &&
+sudo create_ap wlp61s0 wlp61s0 $1 $2
+# $1 = ssid, $2 = ap password
+
